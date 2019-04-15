19985056b96b09ca5a5735111901f35a5dbde580
jeh26033 <jeh26033@email.vccs.edu>
2019-04-15 11:18:35 -0400

    updated height function
    updated

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ $(document).ready(function(){
   $(".quad").mouseover(function(){
     $(this).addClass('active col-md-10').removeClass('col-md-6');
     $(this).siblings().addClass('inactive col-md-2').removeClass('col-md-6'); 
-  });
 
+  }); 
   $(".card-row").mouseover(function(){
     $(this).addClass('activeDiv')
     $(this).siblings().addClass('inactiveDiv'); 
   });
+ 
    $(".card-row").mouseout(function(){
     $(this).removeClass('activeDiv')
     $(this).siblings().removeClass('inactiveDiv'); 
   });
-  
+
+
   $(".quad").mouseout(function(){
     $(this).siblings().removeClass('inactive col-md-2').addClass('col-md-6');
     $(this).removeClass('active col-md-9').addClass('col-md-6');
